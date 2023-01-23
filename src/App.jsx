0d65265fc188958e45f50cf6474d3c86f4590a89
Paulo Felipe <PauloFelipet15@gmail.com>
2023-01-23 20:32:09 -0300

    firs commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  const handleClick = () => {
+    const button = document.querySelector('.nao')
+
+    // gerando novas coordenadas aleatoriamente
+    const newLeft = Math.floor(Math.random() *800);
+    const newTop = Math.floor(Math.random() * 800);
+    const newBottom = Math.floor(Math.random() * 800);
+    const newRigth = Math.floor(Math.random() * 800);
+    // atualizando as coordenadas do botão
+    button.style.position = 'absolute';
+    button.style.left = `${newLeft}px`;
+    button.style.top = `${newTop}px`;
+    button.style.bottom = `${newBottom}px`;
+    button.style.right = `${newRigth}px`;
+
+  }
+
+
+
+
+  return (
+    <div className="App">
+
+      <h1 className='title'>Você quer Namorar Comigo?</h1>
+      <div className="container">
+
+
+        <button className='sim' >Sim</button>
+        <button className='nao' onClick={() => {
+          handleClick()
+        }} >Não</button>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default App
